8832756c5948bdd0dfe85b99337278f00d582d93
caesiumy <mn0658@naver.com>
2019-12-01 23:36:28 +0900

    #2.3 Add key prop

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React from "react";
 
-function Potato(props) {
-  return (
-    <h1>
-      I like {props.name} from {props.country}{" "}
-    </h1>
-  );
-}
-
 const dummy_props = [
   {
+    id: 1,
     name: "ramen",
     country: "japan"
   },
   {
+    id: 2,
     name: "curry",
     country: "india"
   },
   {
+    id: 3,
     name: "sundae",
     country: "korea"
   },
   {
+    id: 4,
     name: "pasta",
     country: "italy"
   }
 ];
+function Potato(props) {
+  return (
+    <h1>
+      I like {props.name} from {props.country}
+    </h1>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
       {dummy_props.map(item => (
-        <Potato name={item.name} country={item.country} />
+        <Potato key={item.id} name={item.name} country={item.country} />
       ))}
     </div>
   );
