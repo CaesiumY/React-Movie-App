c5adea2c6bcb6b919b14c5fe72c8ed2e9fba94f1
caesiumy <mn0658@naver.com>
2019-12-01 22:40:16 +0900

    #2.1 Move Potato component inside & Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Potato(props) {
+  return <h1>I like {props.fav}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      Hello world!
-      <Potato />
+      <h1>Hello world!</h1>
+      <Potato fav="ramen" />
     </div>
   );
 }
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato() {
-  return <div>I am Potato</div>;
-}
-
-export default Potato;
