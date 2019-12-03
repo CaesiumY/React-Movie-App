5a66f64b2921b5093518a0927cf585e03ff28db2
caesiumy <mn0658@naver.com>
2019-12-03 22:26:07 +0900

    #3.3 Reset to begin movie project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("Constructor!");
-  }
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
   componentDidMount() {
-    console.log("Component Mounted!");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    console.log("Component Updated!");
-  }
-
-  state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
 
   render() {
-    console.log("Component Rendered!");
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>My number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        <div>{isLoading ? "Loading..." : "We are ready"}</div>
       </div>
     );
   }
