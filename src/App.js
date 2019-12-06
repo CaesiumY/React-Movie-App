945d682a7b3845fa97950c367b6e4c4d8d44d4e5
caesiumy <mn0658@naver.com>
2019-12-06 23:41:17 +0900

    #4.0 feat: Add axios & GET movie api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { MOVIE_API_KEY } from "./tokens.json";
 
 export default class App extends Component {
   state = {
@@ -6,17 +8,33 @@ export default class App extends Component {
     movies: []
   };
 
+  getMovieData = async () => {
+    const {
+      data: {
+        movieListResult: { movieList }
+      }
+    } = await axios.get(
+      `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${MOVIE_API_KEY}`
+    );
+    console.log(movieList);
+    this.setState({ movies: movieList });
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovieData();
+    this.setState({ isLoading: false });
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
         <div>{isLoading ? "Loading..." : "We are ready"}</div>
+        <div>
+          {movies.map(movie => (
+            <p>{movie.movieNm}</p>
+          ))}
+        </div>
       </div>
     );
   }
