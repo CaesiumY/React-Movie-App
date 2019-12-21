823d23dc98ff92f181635327598cc6163193ad44
caesiumy <mn0658@naver.com>
2019-12-21 15:26:04 +0900

    #6.0 feat: Ready for routing

---
+++ src/routes/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Movie from "../components/Movie";
+import "../css/Home.css";
+
+export default class App extends Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovieData = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovieData();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <p className="loader__text">Loading...</p>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
