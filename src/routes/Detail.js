e75c78409813fc0711ccf281f4838a3ff271aaa9
caesiumy <mn0658@naver.com>
2019-12-23 01:44:44 +0900

    style: Add detail style & Add some style fix

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import "../css/Detail.css";
 
 export class Detail extends Component {
   render() {
@@ -7,10 +8,25 @@ export class Detail extends Component {
     const { location } = this.props;
     if (location.state) {
       return (
-        <div>
-          {location.state.title}
-          <br />
-          {location.state.year}
+        <div className="detail__container">
+          <img
+            className="detail__img"
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <div className="detail__info">
+            <h3 className="detail__title">{location.state.title}</h3>
+            <h5 className="detail__year">( {location.state.year} )</h5>
+            <ul className="detail__genres">
+              {location.state.genres.map((genre, index) => (
+                <li className="genres__genre" key={index}>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="detail__summary">{location.state.summary}</p>
+          </div>
         </div>
       );
     } else {
