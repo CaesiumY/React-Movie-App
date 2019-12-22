487e09601c9e49a57c0cfd97fd72cad092f14784
caesiumy <mn0658@naver.com>
2019-12-22 23:13:53 +0900

    #6.1 feat: Add basic router sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <HashRouter>
+        <Route path="/" exact={true} component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
